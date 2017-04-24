a8a6fee52667674d68679b6aa6a4f0b4a3821b8d
sorinmarkovi <mcdanieln@student.swosu.edu>
2017-04-24 10:42:56 -0500

    adding matrix stuff

--- twoDemArray/src/twoDemArray/First.java
+++ twoDemArray/src/twoDemArray/First.java
@@ -1,22 +1,26 @@
 package twoDemArray;
 
-public class First {
-	static int[][] matrix = new int[16][16];
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-	public static void main(String[] args) 
+public class First 
+{
+	static int[] [] matrix = new int[16][16];
+	public static void main(String[] args) throws IOException 
 	{
 		// TODO find the determ. of a 16x16 matrix
 		determinant(matrix);
+		fillArray();
 	}
-
-	public static int determinant(int[][] matrix) 
+	public static int determinant(int[] [] matrix) 
 	{
 		// code borrowed from http://professorjava.weebly.com/matrix-determinant.html
 		// method sig. takes a matrix (two dimensional array), returns determinant.
 		int sum = 0;
 		int s;
-		if (matrix.length == 1) { // bottom case of recursion. size 1 matrix
-									// determinant is itself.
+		if (matrix.length == 1) 
+		{ // bottom case of recursion. size 1 matrix determinant is itself.
 			return (matrix[0][0]);
 		}
 		for (int i = 0; i < matrix.length; i++) { // finds determinant using
@@ -47,5 +51,47 @@ public class First {
 			sum += s * matrix[0][i] * (determinant(smaller)); // recursive step: determinant of larger determined by smaller.
 		}
 		return (sum); // returns determinant value. once stack is finished, returns final determinant.
+	}
+	public static void fillArray()
+	{
+		String fileName = "m0016x0016.bin";
+
+        try {
+            // Use this for reading the data.
+            byte[] buffer = new byte[1000];
+
+            FileInputStream inputStream = 
+                new FileInputStream(fileName);
+
+            // read fills buffer with data and returns
+            // the number of bytes read (which of course
+            // may be less than the buffer size, but
+            // it will never be more).
+            int total = 0;
+            int nRead = 0;
+            while((nRead = inputStream.read(buffer)) != -1) {
+                // Convert to String so we can display it.
+                // Of course you wouldn't want to do this with
+                // a 'real' binary file.
+                System.out.println(new String(buffer));
+                total += nRead;
+            }   
+            // Always close files.
+            inputStream.close();        
+
+            System.out.println("Read " + total + " bytes");
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
 	}
 }
